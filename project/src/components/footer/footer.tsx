9be7dfbdf4304e488d7f7897f2d1da9f7508f49f
Milkriver <mapav4@yandex.ru>
2023-02-06 16:35:35 +0500

    + ссылки в footer

--- project/src/components/footer/footer.tsx
+++ project/src/components/footer/footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 function Footer(): JSX.Element {
   return (
     <footer className="footer">
@@ -11,25 +13,25 @@ function Footer(): JSX.Element {
           <p className="footer__description">Интернет-магазин фото- и видеотехники</p>
           <ul className="social">
             <li className="social__item">
-              <a className="link" href="#" aria-label="Переход на страницу вконтатке">
+              <Link className="link" to="#" aria-label="Переход на страницу вконтатке">
                 <svg width="20" height="20" aria-hidden="true">
                   <use xlinkHref="#icon-vk"></use>
                 </svg>
-              </a>
+              </Link>
             </li>
             <li className="social__item">
-              <a className="link" href="#" aria-label="Переход на страницу pinterest">
+              <Link className="link" to="#" aria-label="Переход на страницу pinterest">
                 <svg width="20" height="20" aria-hidden="true">
                   <use xlinkHref="#icon-pinterest"></use>
                 </svg>
-              </a>
+              </Link>
             </li>
             <li className="social__item">
-              <a className="link" href="#" aria-label="Переход на страницу reddit">
+              <Link className="link" to="#" aria-label="Переход на страницу reddit">
                 <svg width="20" height="20" aria-hidden="true">
                   <use xlinkHref="#icon-reddit"></use>
                 </svg>
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
@@ -38,20 +40,20 @@ function Footer(): JSX.Element {
             <p className="footer__title">Навигация</p>
             <ul className="footer__list">
               <li className="footer__item">
-                <a className="link" href="#">Каталог
-                </a>
+                <Link className="link" to="#">Каталог
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">Гарантии
-                </a>
+                <Link className="link" to="#">Гарантии
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">Доставка
-                </a>
+                <Link className="link" to="#">Доставка
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">О компании
-                </a>
+                <Link className="link" to="#">О компании
+                </Link>
               </li>
             </ul>
           </li>
@@ -59,16 +61,16 @@ function Footer(): JSX.Element {
             <p className="footer__title">Ресурсы</p>
             <ul className="footer__list">
               <li className="footer__item">
-                <a className="link" href="#">Курсы операторов
-                </a>
+                <Link className="link" to="#">Курсы операторов
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">Блог
-                </a>
+                <Link className="link" to="#">Блог
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">Сообщество
-                </a>
+                <Link className="link" to="#">Сообщество
+                </Link>
               </li>
             </ul>
           </li>
@@ -76,12 +78,12 @@ function Footer(): JSX.Element {
             <p className="footer__title">Поддержка</p>
             <ul className="footer__list">
               <li className="footer__item">
-                <a className="link" href="#">FAQ
-                </a>
+                <Link className="link" to="#">FAQ
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">Задать вопрос
-                </a>
+                <Link className="link" to="#">Задать вопрос
+                </Link>
               </li>
             </ul>
           </li>
