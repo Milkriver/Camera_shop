02edbd68ec22d2f7305119a262b694c528460f90
ChurkinaM <mapav4@yandex.ru>
2023-02-25 12:36:25 +0500

    добавлена 404 страница

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import CatalogPage from '../../pages/catalog-page/catalog-page';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import ProductPage from '../../pages/product-page/product-page';
 
 function App(): JSX.Element {
@@ -13,6 +14,10 @@ function App(): JSX.Element {
       <Route
         path={AppRoute.Product}
         element={<ProductPage />}
+      />
+      <Route
+        path="*"
+        element={<NotFoundScreen />}
       />
     </Routes>
   );
