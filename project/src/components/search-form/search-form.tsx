0bb0679143012c446f24f4d87cd5421b7d866efc
Milkriver <mapav4@yandex.ru>
2023-03-29 09:53:24 +0500

    добавлен сброс поиска

--- project/src/components/search-form/search-form.tsx
+++ project/src/components/search-form/search-form.tsx
@@ -6,6 +6,7 @@ function SearchForm(): JSX.Element {
   const products = useAppSelector(setOffers);
   const [searchValue, setSearchValue] = useState('');
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setSearchValue(event.currentTarget.value);
+  const handleClick = () => setSearchValue('');
   if(!products){
     return <li className="form-search__select-item" tabIndex={0}></li>;
   }
@@ -18,13 +19,13 @@ function SearchForm(): JSX.Element {
           <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
             <use xlinkHref="#icon-lens"></use>
           </svg>
-          <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" onChange={handleChange}/>
+          <input className="form-search__input" type="text" value={searchValue} autoComplete="off" placeholder="Поиск по сайту" onChange={handleChange}/>
         </label>
         <ul className="form-search__select-list">
           {filteredProducts && filteredProducts.map((element) => <li className="form-search__select-item" tabIndex={0} key={element.name}>{element.name}</li>)}
         </ul>
       </form>
-      <button className="form-search__reset" type="reset">
+      <button className="form-search__reset" type="reset" onClick={handleClick}>
         <svg width="10" height="10" aria-hidden="true">
           <use xlinkHref="#icon-close"></use>
         </svg><span className="visually-hidden">Сбросить поиск</span>
