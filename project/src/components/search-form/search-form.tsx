d88b2c043dcd9cd3f647f8d65251a26e84ce2134
Milkriver <mapav4@yandex.ru>
2023-04-03 13:20:35 +0500

    добавлено управление стрелками

--- project/src/components/search-form/search-form.tsx
+++ project/src/components/search-form/search-form.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { generatePath, Link } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from 'react';
+import { generatePath, Link, useNavigate } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { fetchSearchedOffersAction } from '../../store/api-actions';
@@ -7,7 +7,10 @@ import { setSearchedOffers } from '../../store/offer-process/selectors';
 
 function SearchForm(): JSX.Element {
   const searchedProducts = useAppSelector(setSearchedOffers);
+  const navigate = useNavigate();
   const [searchValue, setSearchValue] = useState('');
+  const [focusedItem, setFocusedItem] = useState(0);
+  const [ isFocused, setIsFocused ] = useState(false);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setSearchValue(event.currentTarget.value);
   const handleClick = () => setSearchValue('');
   const dispatch = useAppDispatch();
@@ -23,6 +26,37 @@ function SearchForm(): JSX.Element {
     }
   },[dispatch, nameParams, searchValue]);
 
+  const activeItem = document.querySelectorAll('#search-list-item')[focusedItem];
+  const keyPress = useCallback((event: KeyboardEvent) => {
+    if (event.key === 'Enter' && searchedProducts) {
+      navigate(generatePath(AppRoute.Product, { id: String(searchedProducts[focusedItem].id) }));
+    }
+    if(event.key === 'ArrowUp'){
+      if(searchedProducts){
+        setIsFocused(true);
+        setFocusedItem(focusedItem > 0 ? focusedItem - 1 : 0);
+      }
+    }
+    if(event.key === 'ArrowDown'){
+      if(searchedProducts){
+        setIsFocused(true);
+        setFocusedItem(focusedItem < searchedProducts.length - 1 ? focusedItem + 1 : searchedProducts.length - 1);
+      }
+    }
+  }, [navigate, searchedProducts, focusedItem]);
+
+  useEffect(() => {
+    if(activeItem && isFocused){
+      (activeItem as HTMLInputElement).focus();
+    }
+  },[activeItem, isFocused]);
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyPress);
+    return () => {
+      document.removeEventListener('keydown', keyPress);
+    };
+  }, [keyPress]);
 
   return (
     <div className={`form-search ${searchValue && searchedProducts && searchedProducts.length !== 0 ? 'list-opened' : ''}`}>
@@ -36,7 +70,7 @@ function SearchForm(): JSX.Element {
         <ul className="form-search__select-list">
           {searchedProducts && searchedProducts.map((element) => (
             <li className="form-search__select-item" key={element.name}>
-              <Link to={generatePath(AppRoute.Product, { id: String(element.id) })} onClick={handleClick} tabIndex={0}>{element.name}</Link>
+              <Link to={generatePath(AppRoute.Product, { id: String(element.id) })} id='search-list-item' onClick={handleClick} tabIndex={0}>{element.name}</Link>
             </li>
           ))}
         </ul>
