d5c392221bd306be70371242ecb8ed2e60118602
ChurkinaM <mapav4@yandex.ru>
2023-04-23 17:55:25 +0500

    переход в корзину

--- project/src/components/product-card/product-card.tsx
+++ project/src/components/product-card/product-card.tsx
@@ -79,7 +79,7 @@ function ProductCard({ product, isActive }: IProps): JSX.Element {
         {
           isProductInBasket
             ?
-            <Link className="btn btn--purple-border product-card__btn product-card__btn--in-cart" to={generatePath(AppRoute.Product, { id: String(product.id) })}>
+            <Link className="btn btn--purple-border product-card__btn product-card__btn--in-cart" to={AppRoute.Basket}>
               <svg width="16" height="16" aria-hidden="true">
                 <use xlinkHref="#icon-basket"/>
               </svg>В корзине
