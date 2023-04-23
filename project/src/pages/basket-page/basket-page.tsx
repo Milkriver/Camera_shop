8c477ce0875731c2b60e3d30a60fc20bed01a292
ChurkinaM <mapav4@yandex.ru>
2023-04-23 17:01:58 +0500

    механизм отправки заказа

--- project/src/pages/basket-page/basket-page.tsx
+++ project/src/pages/basket-page/basket-page.tsx
@@ -1,21 +1,56 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AddOrderModalSuccess from '../../components/add-order-modal-success/add-order-modal-success';
 import BasketItem from '../../components/basket-item/basket-item';
 import Breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
+import { AppRoute } from '../../const';
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { addCouponAction } from '../../store/api-actions';
-import { positionsSelector, sumSelector, discountSelector } from '../../store/order-process/selectors';
+import { addCouponAction, addOrderAction } from '../../store/api-actions';
+import { changeErrorStatus, changeOrderStatus } from '../../store/order-process/order-process';
+import { positionsSelector, sumSelector, discountSelector, isOrderSuccessedSelector, hasErrorSelector, couponSelector } from '../../store/order-process/selectors';
 
 function BasketPage(): JSX.Element {
   const dispatch = useAppDispatch();
   const [coupon, setCoupon] = useState('');
-
+  const navigate = useNavigate();
   const basketList = useAppSelector(positionsSelector);
   const totalPrice = useAppSelector(sumSelector);
+  const isOrderSuccessed = useAppSelector(isOrderSuccessedSelector);
+  const hasError = useAppSelector(hasErrorSelector);
+  const positionList = useAppSelector(positionsSelector);
   const discountPercent = useAppSelector(discountSelector);
+  const checkedCoupon = useAppSelector(couponSelector);
   const discountSum = discountPercent * totalPrice / 100;
   const payment = totalPrice - discountSum;
+
+  useEffect(()=> {
+    if(hasError){
+      navigate(AppRoute.Error);
+      dispatch(changeErrorStatus());
+    }
+  },[hasError, navigate, dispatch]);
+
+  useEffect(() => {
+    document.body.style.overflow = (isOrderSuccessed) ? 'hidden' : 'unset';
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isOrderSuccessed]);
+
+  const ecsPress = useCallback((event: { keyCode: number }) => {
+    if (event.keyCode === 27) {
+      dispatch(changeOrderStatus());
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    document.addEventListener('keydown', ecsPress);
+    return () => {
+      document.removeEventListener('keydown', ecsPress);
+    };
+  }, [ecsPress]);
 
   const handleCouponEdit = (event: React.ChangeEvent<HTMLInputElement>) => {
     const couponValue = event.target.value;
@@ -33,6 +68,17 @@ function BasketPage(): JSX.Element {
 
     dispatch(addCouponAction({coupon}));
   };
+
+  const handleOrder = () => {
+    dispatch(addOrderAction(
+      {
+        camerasIds: positionList.map((item) => item.item.id),
+        coupon: checkedCoupon
+      }
+    ));
+  };
+
+  const handleModalClose = () => dispatch(changeOrderStatus());
 
   return(
     <>
@@ -78,12 +124,13 @@ function BasketPage(): JSX.Element {
                     <span className="basket__summary-text basket__summary-text--total">К оплате:</span>
                     <span className="basket__summary-value basket__summary-value--total">{payment} ₽</span>
                   </p>
-                  <button className="btn btn--purple" type="submit">Оформить заказ
+                  <button className="btn btn--purple" onClick={handleOrder}>Оформить заказ
                   </button>
                 </div>
               </div>
             </div>
           </section>
+          {isOrderSuccessed && <AddOrderModalSuccess onClick={handleModalClose} />}
         </div>
       </main>
       <Footer/>
